a37670821b5fafc218af49abfaf1371f733faf96
deepmodak79 <2020itb008.deep@students.iiests.ac.in>
2025-12-20 17:36:01 +0530

    Initial commit: JS fundamentals practice

---
+++ first.js
@@ -0,0 +1,16 @@
+let mainNumber=Number(prompt("Enter the main number from 1 to 10"));
+let userGuess=Number(prompt("Guess the number from 1 to 10"));
+
+//console.log(userGuess);
+while(userGuess!=mainNumber){
+
+if(userGuess<mainNumber){
+    console.log("Number is small u need to enter big number that u have guess")
+}
+else if (userGuess>mainNumber){
+    console.log("the number u gueesed is larger than the actual number")
+}
+    userGuess=Number(prompt("Guess the Number again"));
+}
+
+console.log("your guess is correct:",userGuess);
