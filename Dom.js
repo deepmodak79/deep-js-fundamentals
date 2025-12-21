8299ac1e851c3acb7f592628821f2399264bd581
deepmodak79 <2020itb008.deep@students.iiests.ac.in>
2025-12-21 21:20:44 +0530

    DOM Questions

---
+++ Dom.js
@@ -0,0 +1,7 @@
+//Question 1:- Create a H2 heading element with text- "Hello javascript". Append "from Deep Modak" to text using JSON.
+
+let h2=document.querySelector("h2")
+console.log(h2);
+console.dir(h2);
+console.log(h2.innerText);
+h2.innerText=(h2.innerText) + " from Deep Modak";
